1987ff71718f6c2e46ceb110a936bde168168bd4
Felix Hultman <karlfelixhultman@gmail.com>
2024-01-22 20:23:44 +0100

    stuff

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import express from 'express';
-import expressHandlebars from 'express-handlebars';
+import {engine} from 'express-handlebars';
 
 const app = express();
 
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+
+response.render();
+
 app.use('/static', express.static('./statitc'));
 
-app.listen(3080);
+app.listen(5080);
