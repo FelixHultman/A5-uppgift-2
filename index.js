2b6da8cf3e56e8629ef513f04de0355d5dbb69fa
Felix Hultman <karlfelixhultman@gmail.com>
2024-01-22 22:19:36 +0100

    fixing

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 import express from 'express';
 import {engine} from 'express-handlebars';
+import fs from 'fs/promises';
 
 const app = express();
-
 app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './templates');
 
-response.render();
-
 app.use('/static', express.static('./statitc'));
 
-app.listen(5080);
+app.listen(3080);
